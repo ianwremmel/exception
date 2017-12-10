faa7d27b09c4322bbdb56646360eb1f3fd2474fa
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2017-12-10 14:47:10 -0800

    style(lint): remove backticks where not needed

--- src/index-spec.js
+++ src/index-spec.js
@@ -1,37 +1,37 @@
 'use strict';
 
-const {assert} = require('chai');
+const {assert} = require(`chai`);
 
 /* eslint-env mocha */
 /* eslint-disable require-jsdoc */
 
-const Exception = require('.');
+const Exception = require(`.`);
 
-describe('Exception', () => {
-  it('subclasses Error', () => {
+describe(`Exception`, () => {
+  it(`subclasses Error`, () => {
     assert.instanceOf(new Exception(), Error);
   });
 
-  it('has a stacktrace', () => {
-    assert.property(new Exception(), 'stack');
+  it(`has a stacktrace`, () => {
+    assert.property(new Exception(), `stack`);
   });
 
-  it('stringifies as "Exception" and not "Error"', () => {
+  it(`stringifies as "Exception" and not "Error"`, () => {
     const e = new Exception();
     assert.match(e, /Exception/);
   });
 
-  it('stringifies with its message', () => {
-    const e = new Exception('Something bad happened');
+  it(`stringifies with its message`, () => {
+    const e = new Exception(`Something bad happened`);
     assert.match(e, /Exception: Something bad happened/);
   });
 
-  it('stringifies according to its derived class name', () => {
+  it(`stringifies according to its derived class name`, () => {
     class MyException extends Exception {}
     const e = new MyException();
     assert.match(e, /MyException/);
 
-    const e2 = new MyException('Something worse happened');
+    const e2 = new MyException(`Something worse happened`);
     assert.match(e2, /MyException: Something worse happened/);
   });
 });
